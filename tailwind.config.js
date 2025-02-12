c209d7b262c577169676d69e911a822c6f8e56f8
Matthis <matthisphan.pro@gmail.com>
2025-02-12 22:18:14 +0100

    fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     content: [
     "./src/app/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
+	"./src/layout/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
   	extend: {
