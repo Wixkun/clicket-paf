c91924740396a2de1adc32dddbae9299be8ebb83
Wixkun <alexismontoya2004@gmail.com>
2025-02-12 21:15:40 +0100

    merge

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
         url: "https://www.clicket-paf.com/images/cover.jpg",
         width: 1200,
         height: 630,
-        alt: "ClickEtPaf - Histoires absurdes"
+        alt: "ClickEtPaf"
       }
     ]
   },
